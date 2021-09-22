4af1c7f1892fda1ab813aa9fc4caf0577f6367fc
Vlad <VladDaImpaler@gmail.com>
2021-09-22 00:31:46 -0400

    model validations, custom validation, and placeholder _form

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,8 @@
 class Meal < ApplicationRecord
+    validates :title, presence: true
+    validates :title, uniqueness: true
+    validates_with MealsValidator, fields: [:instructions, :ingredients]
+
+    #  title:sting, category:sting, area:sting, instructions:json image:binary, tags:json, ingredients:json, source:string
+    
 end
