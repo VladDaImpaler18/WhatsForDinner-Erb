96fe66f92e2c4093815a5f2b426900e777a78ee9
vlad <VladDaImpaler@gmail.com>
2021-09-25 23:29:21 -0400

    functional meals#import feature

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -3,23 +3,35 @@ class MealsController < ApplicationController
   end
 
   def show
-    title = params.require(:title).downcase
-    @meal = Meal.find_by(:title=>title)
+    # title = params.require(:title).downcase
+    # @meal = Meal.find_by(:title=>title)
+    binding.pry
   end
 
   def discover
+  end
+
+  def new
+    @meal = Meal.new
   end
 
   def import
     url = params.require(:url)
     data = Scrapers::FoodNetwork.grab(url)
     args={}
-    Meal.new.attributes.symbolize_keys.each { |k,v| args[k]=data[k]}
-    @meal = Meal.new(args) || Meal.new
-    
+    Meal.new.attributes.symbolize_keys.each { |k,v| args[k]=data[k]} #Shared, only the keys that Meals has will be copied over
+    @meal = Meal.new(args)
+    # redirect_to new_meal_path(meal)
+    render 'meals/new'
   end
 
   def create
+    meal = Meal.new(meal_params(:title,:category,:area,:source,:instructions => [],:tags => [],:ingredients => {}))
+    if newMeal.save
+      render json: newMeal
+    else
+      render json: newMeal.errors.messages
+    end
   end
 
   def update
