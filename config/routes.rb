c1a82063f616f2e46e4f471d964e058629bd919a
vlad <VladDaImpaler@gmail.com>
2021-09-25 23:30:39 -0400

    changed to meal resources [create, new, update, destroy, show]

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'meals/index' 
-  get '/meals/:title' => "meals#show", as: 'meals_show'
+  resources :meals, only: [:create, :new, :show, :update, :destroy]
   get '/discover' => "meals#discover", as: 'meals_discover'
-  post '/import/' => "meals#import", as: 'meals_import'
-  post '/meals/create' => "meals#create", as: 'meals_create'
-  patch 'meals/update'
-  delete 'meals/destroy'
+
+  # get 'meals/index' 
+  # get '/meals/:title' => "meals#show", as: 'meals_show'
+  post '/import/new' => "meals#import", as: 'meals_import'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
